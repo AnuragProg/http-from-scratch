4e2feb6a689119045817776195e6534b7cd36e48
Anurag Singh <anuragcollege7@gmail.com>
2024-05-25 21:13:59 +0545

    pass stage

--- app/server.go
+++ app/server.go
@@ -9,23 +9,7 @@ import (
 )
 
 
-func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
-
-	// Uncomment this block to pass the first stage
-
-	l, err := net.Listen("tcp", "0.0.0.0:4221")
-	if err != nil {
-		fmt.Println("Failed to bind to port 4221")
-		os.Exit(1)
-	}
-
-	conn, err := l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
-	}
+func handleConn(conn net.Conn){
 
 	data := make([]byte, 1024)
 	conn.Read(data)
@@ -51,4 +35,27 @@ func main() {
 		response = []byte("HTTP/1.1 404 Not Found\r\n\r\n")
 	}
 	conn.Write(response)
+}
+
+func main() {
+	// You can use print statements as follows for debugging, they'll be visible when running tests.
+	fmt.Println("Logs from your program will appear here!")
+
+	// Uncomment this block to pass the first stage
+
+	l, err := net.Listen("tcp", "0.0.0.0:4221")
+	if err != nil {
+		fmt.Println("Failed to bind to port 4221")
+		os.Exit(1)
+	}
+
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+
+		go handleConn(conn)
+	}
 }
