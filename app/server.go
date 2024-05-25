baa5cdff40a8162602b663c9607273aabfe1b45f
Anurag Singh <anuragcollege7@gmail.com>
2024-05-25 20:56:14 +0545

    pass stage

--- app/server.go
+++ app/server.go
@@ -8,41 +8,6 @@ import (
 	"os"
 )
 
-func handleExtractUrl(conn net.Conn){
-	data := make([]byte, 1024)
-	conn.Read(data)
-
-	headers :=strings.Split(string(data), "\r\n") 
-	requestInfo := strings.Split(headers[0], " ")
-	fmt.Println(requestInfo)
-	fmt.Println(string(data))
-
-	var response []byte
-	switch requestInfo[1]{
-		case "/":
-			response = []byte("HTTP/1.1 200 OK\r\n\r\n")
-		default:
-			response = []byte("HTTP/1.1 404 Not Found\r\n\r\n")
-	}
-	conn.Write(response)
-}
-
-func handleRespondWithBody(conn net.Conn){
-	data := make([]byte, 1024)
-	conn.Read(data)
-
-	route := strings.Split(strings.Split(string(data), "\r\n")[0], " ")[1]
-
-	if !strings.HasPrefix(route, "/echo/") {
-		conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
-		return
-	}
-
-	echoData := strings.TrimPrefix(route, "/echo/")
-
-	response := []byte(fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %v\r\n\r\n%v", len(echoData), echoData))
-	conn.Write(response)
-}
 
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -61,5 +26,21 @@ func main() {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
-	handleRespondWithBody(conn)
+
+	data := make([]byte, 1024)
+	conn.Read(data)
+
+	route := strings.Split(strings.Split(string(data), "\r\n")[0], " ")[1]
+
+	var response []byte
+	switch {
+		case route == "/":
+			response = []byte("HTTP/1.1 200 OK\r\n\r\n")
+		case strings.HasPrefix(route, "/echo"):
+			echoData := strings.TrimPrefix(route, "/echo/")
+			response = []byte(fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %v\r\n\r\n%v", len(echoData), echoData))
+		default:
+			response = []byte("HTTP/1.1 404 Not Found\r\n\r\n")
+	}
+	conn.Write(response)
 }
